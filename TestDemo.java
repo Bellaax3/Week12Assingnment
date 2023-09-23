97df256049d83f9ee71a0dbe02ea00874805d4f7
Bellaax3 <139819342+Bellaax3@users.noreply.github.com>
2023-09-22 21:08:34 -0400

    Add files via upload

---
+++ TestDemo.java
@@ -0,0 +1,28 @@
+package com.promineotech;
+
+import java.util.Random;
+
+public class TestDemo {
+	
+	public int addPositive(int a, int b) {
+		
+		if(a>0 && b>0) {
+			int sum = a + b;
+			return sum;
+		} else {
+			throw new IllegalArgumentException("Both parameters must be positive!");
+		}
+	}
+	
+	public int randomNumberSquared() {
+		int randomNumber = getRandomInt();
+		int result = randomNumber * randomNumber;
+		return result;
+	}
+
+	int getRandomInt() {
+		Random random = new Random();
+		return random.nextInt(10) + 1;
+	}
+
+}
